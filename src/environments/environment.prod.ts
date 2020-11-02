a89ddf09262b2183c8b516d10f681b1c745de0fe
Dominic Dopico <serafin.dopico@gmail.com>
2020-11-02 12:38:57 -0800

    firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCbx_V0ViDhcHuGlvCXCo432MUzMz8fHjc",
+    authDomain: "angular-firestarter-bcaee.firebaseapp.com",
+    databaseURL: "https://angular-firestarter-bcaee.firebaseio.com",
+    projectId: "angular-firestarter-bcaee",
+    storageBucket: "angular-firestarter-bcaee.appspot.com",
+    messagingSenderId: "1034458802739",
+    appId: "1:1034458802739:web:5595546013ccf4c4549146",
+    measurementId: "G-V2Q74QM1YC"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCbx_V0ViDhcHuGlvCXCo432MUzMz8fHjc",
+    authDomain: "angular-firestarter-bcaee.firebaseapp.com",
+    databaseURL: "https://angular-firestarter-bcaee.firebaseio.com",
+    projectId: "angular-firestarter-bcaee",
+    storageBucket: "angular-firestarter-bcaee.appspot.com",
+    messagingSenderId: "1034458802739",
+    appId: "1:1034458802739:web:5595546013ccf4c4549146",
+    measurementId: "G-V2Q74QM1YC"
+  }
 };
 
 /*
