5cc6852ce81b57e924228c136c1e8eeea94b607b
Dominic Dopico <serafin.dopico@gmail.com>
2020-11-02 15:57:05 -0800

    signin directive and login page build

---
+++ src/app/user/google-signin.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, HostListener } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+
+@Directive({
+  selector: '[appGoogleSignin]'
+})
+export class GoogleSigninDirective {
+
+  constructor(private afAuth: AngularFireAuth) { }
+
+  @HostListener('click')
+  onclick(): void {
+    this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+}
