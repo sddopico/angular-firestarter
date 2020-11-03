8d489a4d4763db67e05785e2d1c9ba6bb7f75507
Dominic Dopico <serafin.dopico@gmail.com>
2020-11-03 15:34:50 -0800

    database service for kanban boards

---
+++ src/app/kanban/board.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import firebase from 'firebase/app';
+import { switchMap, map } from 'rxjs/operators';
+import { Board, Task } from './board.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+
+  constructor(private afAuth: AngularFireAuth, private db: AngularFirestore) { }
+
+  /**
+   * Creates a new board for the current User
+   */
+  // tslint:disable-next-line: typedef
+  async createBoard() {
+    const user = await this.afAuth.currentUser;
+    return this.db.collection('boards').add({
+      ...DataCue,
+      uid: user.uid,
+      tasks: [{ description: 'Hello!', label: 'yellow' }]
+    });
+  }
+
+
+  /**
+   * Delete board
+   */
+  // tslint:disable-next-line: typedef
+  deleteBoard(boardId: string) {
+    return this.db
+      .collection('boards')
+      .doc(boardId)
+      .delete();
+  }
+
+
+  /**
+   * Update task on board
+   */
+  // tslint:disable-next-line: typedef
+  updateTasks(boardId: string, tasks: Task[]) {
+    return this.db
+      .collection('boards')
+      .doc(boardId)
+      .update({ tasks });
+  }
+
+
+  /**
+   * Remove a specific task from boad
+   */
+  // tslint:disable-next-line: typedef
+  removeTask(boardId: string, task: Task) {
+    return this.db
+      .collection('boards')
+      .doc(boardId)
+      .update({
+        tasks: firebase.firestore.FieldValue.arrayRemove(task)
+      });
+  }
+
+  /**
+   * Get all boards owned by current User
+   */
+  // tslint:disable-next-line: typedef
+  getUserBoards() {
+    return this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.db
+            .collection<Board>('boards', ref =>
+            ref.where('uid', '==', user.uid).orderBy('priority')
+            )
+            .valueChanges({ idField: 'id' });
+        } else {
+          return [];
+        }
+      })
+    );
+  }
+
+
+  /**
+   * Batch write to change the priority of each board for sorting
+   */
+  // tslint:disable-next-line: typedef
+  sortBoards(boards: Board[]) {
+    const db = firebase.firestore();
+    const batch = db.batch();
+    const refs = boards.map(b => db.collection('boards').doc(b.id));
+    refs.forEach((ref, idx) => batch.update(ref, { priority: idx }));
+    batch.commit();
+  }
+}
